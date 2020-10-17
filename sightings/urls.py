8c33886f8b7a339a0d242840d0fc7e01242310bb
Zichan Liu <72879595+ZL2947@users.noreply.github.com>
2020-10-17 18:23:30 +0800

    Update urls.py

--- sightings/urls.py
+++ sightings/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('',views.homepage),
-        path('map/',views.map),
-        path('sightings/',views.sighting),
-        path('sightings/add/',views.add_squirrel),
-        path('sightings/<str:Unique_Squirrel_Id>/',views.update),
+        path('',views.homepage, name='Add'),
+        path('map/',views.map, name='Map'),
+        path('sightings/',views.sighting, name='AllSquirrels'),
+        path('sightings/add/',views.add_squirrel, name='Add'),
+        path('sightings/<str:Unique_Squirrel_Id>/',views.update, name='Update'),
+        path('sighting/stats/',views.stats, name ='Stats')
         ]
