ecf4a4df34f59ddf57da75b81f2b7ac79dbf7397
Zichan Liu <72879595+ZL2947@users.noreply.github.com>
2020-10-17 18:28:07 +0800

    Update views.py

--- sightings/views.py
+++ sightings/views.py
@@ -50,3 +50,35 @@ def update(request,Unique_Squirrel_Id):
             'form':form,
             }
     return render(request, 'sightings/update.html', context)
+
+def stats(request):
+    squirrels = Sight.objects.all()
+    squirrel_count = len(squirrels)
+    adult = Sight.objects.filter(Age='Adult').aggregate(Count('Unique_Squirrel_ID'))['Unique_Squirrel_ID_count']
+    am_shift = Sight.objects.filter(Shift='AM').aggregate(Count('Unique_Squirrel_ID'))['Unique_Squirrel_ID_count']
+    pm_shift = Sight.objects.filter(Shift='PM').aggregate(Count('Unique_Squirrel_ID'))['Unique_Squirrel_ID_count']
+    running = Sight.objects.filter(Running=True).aggregate(Count('Unique_Squirrel_ID'))['Unique_Squirrel_ID_count']
+    climbing = Sight.objects.filter(Climbing=True).aggregate(Count('Unique_Squirrel_ID'))['Unique_Squirrel_ID_count']
+    eating = Sight.objects.filter(Eating=True).aggregate(Count('Unique_Squirrel_ID'))['Unique_Squirrel_ID_count']
+    foraging = Sight.objects.filter(Foraging=True).aggregate(Count('Unique_Squirrel_ID'))['Unique_Squirrel_ID_count']
+    kuks = Sight.objects.filter(Kuks=True).aggregate(Count('Unique_Squirrel_ID'))['Unique_Squirrel_ID_count']
+    quaas = Sight.objects.filter(Quaas=True).aggregate(Count('Unique_Squirrel_ID'))['Unique_Squirrel_ID_count']
+    moans = Sight.objects.filter(Moans=True).aggregate(Count('Unique_Squirrel_ID'))['Unique_Squirrel_ID_count']
+    tail_flags = Sight.objects.filter(Tail_Flags=True).aggregate(Count('Unique_Squirrel_ID'))['Unique_Squirrel_ID_count']
+    tail_twitches = Sight.objects.filter(Tail_Twitches=True).aggregate(Count('Unique_Squirrel_ID'))['Unique_Squirrel_ID_count']
+    context = {
+        'Total': squirrel_count,
+        'Adult Squirrels': adult,
+        'AM Shifts': am_shift,
+        'PM Shifts':pm_shift,
+        'Running Squirrels':running,
+        'Climbing Squirrels': climbing,
+        'Eating Squirrels': eating,
+        'Foraging Squirrels': foraging,
+        'Squirrels with Kuks': kuks,
+        'Squirrels with Quaas': quaas,
+        'Squirrels with Moans': moans,
+        'Squirrels with flagging tail': tail_flags,
+        'Squirrels with twitching tail': tail_twitch
+        }
+    return render(request, 'sightings/stats.html', context)
